a9e944a53cdc3af649250cba1b28458cabcf2712
Juan Carlos M <Juan Carlos M>
2019-05-07 14:25:01 -0500

    curriculum

--- templet/curriculums.php
+++ templet/curriculums.php
@@ -14,14 +14,17 @@
                    	</div>
 
   </div>-->
-
+<br>
+<br>
 <div class="container"><!-- Container -->
                <h2 class="border-title aligncenter jmy_web_div" data-page="nosotros" id="hola77"data-editor="no">
-<?php $this->pnt('hola77','Curriculos'); ?>
-</h2>
+                <?php $this->pnt('hola77','Curriculos'); ?>
+                </h2>
+                <br><br>
                  <font color=black><h4 class="aligncenter jmy_web_div" data-page="nosotros" id="hola78"data-editor="no">
-<?php $this->pnt('hola78',''); ?>
-</h4></font>
+                    <?php echo "<br>"; $this->pnt('hola78',''); ?>
+                    </h4>
+                </font>
          <div class="clear"></div>
  </div> 				
 <div class="portfolio dt-sc-one-fourth column no-space all-sorte">
@@ -34,52 +37,42 @@
                                         <ul>
                                           <li><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>images/portfolio-images/portfolio-img3.jpg" data-gal="prettyPhoto[gallery]" class="button wpshop-cart-button add_to_cart_button product_type_simple"></i></a></li>
                                             <li><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>portfolio-details.html" ></a></li>
-                    </ul>
+                                         </ul>
                                     </div>
                                 </figcaption>
                             </figure>
-                        <figure>
+                            <br>
+                            <br>
+                            <figure id="curricul">
                                 <img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>images/gutierrez.fw.png" alt="portfolio2" title="">
                                 <figcaption>
                                     <div class="fig-content">
                                         <ul>
                                           <li><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>images/portfolio-images/portfolio-img3.jpg" data-gal="prettyPhoto[gallery]" class="button wpshop-cart-button add_to_cart_button product_type_simple"></i></a></li>
                                             <li><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>portfolio-details.html" ></a></li>
-                    </ul>
+                                        </ul>
                                     </div>
                                 </figcaption>
                             </figure>
                             <div class="portfolio dt-sc-one-fourth column no-space all-sorte">
                             
                         </div>
-                        <figure>
+                        <figure id="curricul2">
+                        <br> <br>
                                 <img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>images/ivanalan.png" alt="portfolio2" title="">
                                 <figcaption>
                                     <div class="fig-content">
                                         <ul>
                                           <li><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>images/portfolio-images/portfolio-img3.jpg" data-gal="prettyPhoto[gallery]" class="button wpshop-cart-button add_to_cart_button product_type_simple"></i></a></li>
                                             <li><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>portfolio-details.html" ></a></li>
-                    </ul>
+                                         </ul>
                                     </div>
                                 </figcaption>
                             </figure>
 
-                        <div class="portfolio dt-sc-one-fourth column no-space all-sorte">
-                            
-                        </div>
+                        <br> <br> <br>
 
-                        <div class="portfolio dt-sc-one-fourth column no-space all-sorte">
-                           
-                        </div>
-
-
-                        <div class="portfolio dt-sc-one-fourth column no-space all-sorte">
-                           
-                        </div>
-
-                        <div class="portfolio dt-sc-one-fourth column no-space all-sorte">
-                           
-                        </div>
+                        
                        
                         </div>
              	</div> 				
